a0d66f0a7a92432b9dae6fc880188c170e30a73c
sebastianQuesada1234 <squesada@coppel.com.ar>
2023-12-04 11:50:42 -0300

    subiendo banner doble

---
+++ react/banner.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import style from './banner.css'
+
+
+
+/*
+function banner() {
+    return (
+
+
+        <div className={`${style.containerBanner}`}>
+            <div>
+                <img src={`../assets/navidad/tecnologiaM.png`}  style={background-image: url('../assets/navidad/tecnologiaM.png')} className={`${style.img1}`}></img>
+                <div className={`${style.img1}`} ></div>
+                <div className={`${style.h1img}`}><h1>Innovación a tu alcance</h1>
+                    <div className={`${style.h2img}`}><h2>Descubrí la mejor tecnología en un solo lugar</h2>
+                        <a href="" className={`${style.aRefimg}`}>Ver Todo</a>
+                    </div>
+                </div>
+
+
+            </div>
+            <div>
+                <img src={`../assets/navidad/tecnologiaM.png`} className={`${style.img2}`}></img>
+                <div className={`${style.h1img}`}><h1>Electrodomésticos para el hogar</h1>
+                    <div className={`${style.h2img}`}><h2>Equipá tus espacios</h2>
+                        <a href="" className={`${style.aRefimg}`}>Ver Todo</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default banner
+*/
