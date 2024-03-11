051f42b8eb8814fb82ab1671a572e76a436cc9e9
mcamino-coppel <117738300+mcamino-coppel@users.noreply.github.com>
2024-03-11 17:28:41 -0300

    privacidad

---
+++ react/Privacidad.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+function PoliticaPrivacidadDatos() {
+  return (
+    <div>
+      <h1>Política de Privacidad de Datos:</h1>
+      <p>
+        En Coppel S.A (en adelante, “Coppel”) asumimos el compromiso de tratar los Datos Personales (conforme se define más adelante) que se proporcione en el sitio web www.coppel.com.ar (en adelante, el “Sitio Web”) o a través de cualquier canal existente o a crearse, siempre de conformidad con la Ley de Protección de los Datos Personales N° 25.326 y su normativa complementaria (y aquella que la modifique o reemplace en el futuro). En tal sentido, mediante la presente Política de Privacidad de Datos (que forma parte de los Términos y Condiciones Generales) se informa a los clientes de Coppel que utilicen el Sitio Web o cualquier otro canal de Coppel (en adelante, los “Usuarios”) sobre el tratamiento de sus Datos Personales.
+      </p>
+      <h2>Datos Personales del Usuario</h2>
+      <p>
+        Se entenderá por Datos Personales a la información de cualquier tipo referida a personas físicas o de existencia ideal determinadas o determinables. Coppel recopilará  Datos Personales tales como nombre, apellido, teléfono, domicilio, correo electrónico y, caso de ser necesario, los datos de la tarjeta de crédito.  Coppel se reserva el derecho de solicitar datos adicionales con el fin de validar la identidad del usuario. El Usuario se compromete a brindar sus datos de forma fidedigna y asumiendo la obligación de mantener actualizados sus datos; cualquier daño o perjuicio que acontezca como consecuencia de éste incumplimiento será responsabilidad exclusiva del Usuario.  En caso de advertir que el usuario ha ingresado datos falsos, incompletos o inexactos al Sitio Web, Coppel podrá eliminar dicha información sin notificación previa al Usuario.
+        <br/>
+        Asimismo, se podrá recolectar datos no personales de manera automatizada en el momento en que el Usuario aporta su información personal en el Sitio Web y/o también en cada ingreso al Sitio Web, a través de los canales disponibles, como así también cuando éste actualiza o modifique dicha información. En ciertas ocasiones, Coppel podrá recolectar datos electrónicamente de forma no automatizada al tomar contacto directo con el Usuario.
+      </p>
+      <h2>Uso de los Datos Personales</h2>
+      <p>
+        El Sitio Web requiere del uso de datos del Usuario para poder prestar los servicios ofrecidos en la plataforma, de manera adecuada y eficiente; por tal motivo, los datos otorgados por el Usuario serán utilizados para la mejora de procesos y servicios comerciales, actualizar los registros de Coppel, establecer canales de contacto con los usuarios y/o para cualquier ofrecimiento, como podrá ser promociones y/o acciones de marketing directo que Coppel por sí o a través de terceros decida efectuar, y también para fines estadísticos.
+        <br/>
+        En tal sentido, también podrá ejecutar y validar las transacciones de compra, proceder a la facturación de las compras realizadas, realizar análisis de riesgo, analizar hábitos de consumo y elaborar perfiles de clientes con fines promocionales, comerciales o publicitarios. Analizar acciones y adoptar medidas tendientes a mejorar la experiencia de compra de los Usuarios.   
+        <br/>
+        Coppel no solicitará información que sea incompatible con la finalidad de sus actividades.
+      </p>
+      <h2>La Cuenta del Usuario y su responsabilidad</h2>
+      <p>
+        El Usuario conoce, comprende y acepta utilizar el Sitio Web de buena fe, conforme a lo establecido en la presente Política de Privacidad de Datos y la legislación aplicable. El Usuario declara que conoce, comprende y acepta que el uso y acceso al Sitio Web será su responsabilidad; es decir, que el Usuario debe ser responsable en su accionar ya que ello puede llegar a   perjudicar y/o dañar y/o afectar el normal funcionamiento del Sitio Web o afectar cualquier derecho de Coppel y/o de un tercero de forma directa o indirectamente. Asimismo, Coppel prohíbe el uso del Sitio Web con fines ilegales y/o ilícitos.
+        <br/>
+        El Usuario declara que conoce, comprende y acepta que los servicios que brinda la plataforma del Sitio Web sólo están disponibles para personas mayores de edad y con capacidad legal para contratar. Por consiguiente, las personas que no cumplan con estas condiciones deberán abstenerse de facilitar información y/o dato alguno al Sitio Web. 
+        <br/>
+        Para poder operar en el Sitio Web, será necesario que el usuario se registre en el Sitio Web, indicando su casilla personal de correo electrónico. Posteriormente, deberá facilitar la información que se indica bajo el título “Datos Personales del Usuario” y generará un ingreso con clave. El Sitio Web podrá requerir validaciones de seguridad en relación con la información brindada por el Usuario al momento de la inscripción. En caso de que la información brindada no pueda validarse, el Sitio Web se reserva el derecho de no dar de alta a ese Usuario.El Usuario es el titular exclusivo de su clave de ingreso, la cual es intransferible y de carácter confidencial, por lo tanto, no deberá compartir ni divulgar con terceros su clave de acceso, en consecuencia, cualquier actividad realizada en la cuenta personal, es responsabilidad del Usuario; como así también  informar a sugerenciasweb@coppel.com.ar,  cualquier anomalía o uso no autorizado a Coppel.  
+        <br/>
+        En su cuenta personal el Usuario podrá verificar, modificar y eliminar los Datos Personales que considere necesarios. Asimismo, el Sitio Web  cuenta con una sección de autenticación para que el Usuario cuente con la oportunidad de modificar su contraseña  las veces que considere necesario.  Como medida adicional de seguridad cuenta con sección de gestión de sesiones con el objetivo de brindar información de las sesiones que inició el Usuario.
+        <br/>
+        Coppel se reserva el derecho de inhabilitar y/o cancelar la cuenta del usuario, toda vez que advierta que se vulnera alguna de las estipulaciones indicadas en la presente Política de Privacidad de Datos o cualquier actividad ilícita.
+      </p>
+      <h2>Consentimiento para el uso de los Datos Personales</h2>
+      <p>
+        El Usuario autoriza y presta su consentimiento previo, expreso e informado en los términos de la Ley N.º 25.326 y normativa complementaria, para que los datos recabados  sean almacenados en una base de datos cuya titularidad es exclusiva de Coppel con domicilio en la calle Viamonte 1145 Piso 8 Oficina A, de la Ciudad Autónoma de Buenos Aires (1053).
+        <br/>
+        Coppel se reserva el derecho de inhabilitar y/o cancelar la cuenta del usuario, toda vez que advierta que se vulnera alguna de las estipulaciones indicadas en la presente Política de Privacidad de Datos o cualquier actividad ilícita.
+      </p>
+      <h2>Derechos de los Usuarios</h2>
+      <p>
+        (a) Derecho de información: los Usuarios pueden consultar a la Agencia de Acceso a la Información Pública (o a la autoridad de contralor que la reemplace en el futuro), en forma gratuita, sobre la existencia de archivos, registros o bases de datos, sus finalidades y la identidad de sus responsables. Dicho organismo tiene a su cargo el Registro Nacional de Bases de Datos que es de acceso público gratuito.
+        <br/>
+        (b) Derecho de acceso: cualquier Usuario, titular de Datos Personales, puede ejercer el derecho de acceso a sus Datos Personales en forma gratuita, con intervalos no inferiores a seis (6) meses salvo que se acredite un interés legítimo al efecto. 
+        <br/>
+        (c) Derecho de rectificación, actualización, supresión y confidencialidad: el Usuario tiene derecho a que sean rectificados, actualizados, suprimidos o sometidos a confidencialidad, en forma gratuita, sus Datos Personales de conformidad a lo dispuesto en el artículo 16 de la ley 25.326. 
+        <br/>
+        (d) Derecho de retiro o bloqueo: el Usuario podrá́ solicitar en forma gratuita el retiro o bloqueo de su nombre y correo electrónico de los archivos o bancos de datos con fines de publicidad (artículo 27 inciso 3 de la ley 25.326).
+        Para ejercer cualquiera de estos derechos, el Usuario deberá comunicarse telefónicamente al Centro de Atención Coppel (0800-12-267735) en el horario de Lunes a Viernes de 8:30 a 21 hs o enviando un correo electrónico a atencionarg@coppel.com.ar  detallando su nombre y apellido, número de DNI, su solicitud y razones del ejercicio de su derecho.
+        <br/><br/>
+        La Agencia de Acceso a la Información Pública, en su carácter de Órgano de Control de la Ley N° 25.326, tiene la atribución de atender las denuncias y reclamos que interpongan quienes resulten afectados en sus derechos por incumplimiento de las normas vigentes en materia de protección de datos personales. 
+      </p>
+      <h2>Transferencias de Datos Personales</h2>
+      <p>
+        El Usuario declara que conoce, comprende y acepta que Coppel transfiera sus Datos Personales a Grupo Coppel y/o terceros prestadores vinculados a Coppel por acuerdos comerciales a fin de acceder a los distintos servicios y/o productos prestados por las mismas. El Usuario presta su consentimiento para dicha transferencia incluso si se realiza a países que no brindan los mismos niveles de protección en la materia de Datos Personales que Argentina. 
+        <br/>
+        Asimismo, se informa al Usuario que Coppel podrá tercerizar el servicio de marketing con el fin de que el Usuario reciba oferta de productos y/o servicios y/o ofertas promocionales por parte de éstas. En tal sentido, también podrá divulgar o transferir datos personales con socios comerciales que presten servicios técnicos de procesamiento de datos, de mantenimiento, actualización, almacenamiento o seguridad, del Sitio Web u otros servicios que sean necesarios para mantener el Sitio activo y en funcionamiento. Éstos terceros prestadores pueden estar ubicados en países que no cuenten con normativa acorde en materia de protección de Datos Personales, no obstante se  tomará los recaudos  necesarios en la medida de sus posibilidades para que los Datos Personales se encuentren protegidos en concordancia con la Ley N° 25.326. 
+      </p>
+      <h2>Cookies</h2>
+      <p>
+        Coppel informa que utiliza la tecnología Cookie o el rastreo IP (en adelante “Cookie”) en el Sitio Web con el objeto de acceder a cierta información del Usuario (“Información No Específica”).  Las Cookies son necesarias ya que posibilitan  la navegación del Usuario en la plataforma del Sitio Web.  
+        <br/>
+        Las cookies son mínimas unidades de archivos que guardan la información que generan los Usuarios cada vez que navegan por la web y son preservadas en los navegadores.
+        <br/>
+        La Información No Específica que recopilamos consiste en información sobre los intereses del Usuario, las secciones de nuestro Sitio Web que el Usuario visita y toda otra información similar. La Información No Específica es utilizada solamente con el fin de conocer cuáles son las áreas más solicitadas o preferidas del Sitio Web, qué tecnología se está utilizando, con el objeto de optimizar nuestros servicios. 
+      </p>
+      <h2>Aviso sobre seguridad</h2>
+      <p>
+        Para Coppel es importante resguardar de forma segura y confidencial los datos y operaciones de nuestros Usuarios, por lo tanto, nunca le pediremos que nos informe su clave o algún dato que se encuentre relacionada con transacciones en dinero, por medio de mails, teléfono, redes sociales y/o por cualquier otro medio.
+        <br/>
+        De tal manera, informamos a los Usuarios que nuestro Sitio Web es seguro y Coppel opera únicamente  a través de cuentas que se encuentran verificadas, y que podrán visualizarlas siempre con una tilde azul. 
+        <br/>
+        Por tal razón, es necesario que  ante cualquier contacto sospechoso, nuestros Usuarios no brinden ningún Dato Personal y de forma inmediata lo bloqueen y lo reporten; ya que es muy probable que se esté frente a una situación de ciberdelito.  El Usuario deberá denunciarlo ante la Unidad Fiscal Especializada en Ciberdelincuencia contactándose al teléfono: (5411) 5071-0040 o por a través del siguiente enlace  https://www.mpf.gob.ar/ufeci.
+        <br/>
+        En este sentido, se reitera que Coppel ha implementado las medidas técnicas y organizativas necesarias con el objetivo de garantizar la seguridad y confidencialidad de los Datos Personales de acuerdo a la normativa con el fin de evitar la pérdida, mal uso, alteración, acceso no autorizado y demás riesgos posibles a los datos facilitados. Si bien hemos adoptado todas las medidas conducentes para la seguridad de los datos,  el Usuario deberá ser consciente de que las medidas de seguridad en internet no son inexpugnables. Si el Usuario cree que su interacción con Coppel ya no es segura, deberá notificar a Coppel inmediatamente del problema comunicándose telefónicamente al Centro de Atención Coppel (0800-12-267735) en el horario de Lunes a Viernes de 8:30 a 21 hs o enviando un correo electrónico a atencionarg@coppel.com.ar. 
+        </p>
+    </div>
+  );
+}
+
+export default PoliticaPrivacidadDatos;
